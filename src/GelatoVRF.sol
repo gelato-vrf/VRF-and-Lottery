40781840da5998c14ead09c0cf42df57b709371f
Alberto Centonze <albicento.ac@gmail.com>
2023-04-16 10:44:06 +0200

    GelatoVRF

---
+++ src/GelatoVRF.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract GelatoVRF {
+    uint256 private rng;
+    address public gelatoOp;
+    uint256 lastUpdate;
+
+    constructor(address _gelatoOp) {
+        gelatoOp = _gelatoOp;
+    }
+
+    function setRandom(uint256 _rng) external {
+        require(msg.sender == gelatoOp, "caller is not the gelato operator");
+        rng = _rng;
+    }
+
+    function getRandom(string calldata space) external view returns (uint256) {
+        bytes memory concat = abi.encodePacked(space, rng);
+        uint256 hash = uint256(keccak256(concat));
+        return hash;
+    }
+}
